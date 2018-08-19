c2f2f232b4ea2f86d44045e9d6296905bff933e5
Shrutikatyal <shrutikatyal1997@gmail.com>
2018-08-19 12:44:40 +0530

    Create Student_Person.cpp
    You are given two classes, Person and Student, where Person is the base class and Student is the derived class. Completed code for Person and a declaration for Student are provided for you in the editor. Observe that Student inherits all the properties of Person.
    
    Complete the Student class by writing the following:
    
    A Student class constructor, which has  parameters:
    firstName - A string denoting the Person's first name.
            *   lastName - A string denoting the Person's last name.
            *   id - An integer denoting the Person's ID number.
            *   scores - An array of integers denoting the Person's test scores
    A char calculate() method that calculates a Student object's average and returns the grade character representative of their calculated average:
     if(avg<=100 && avg>=90)
                    return 'O';
                if(avg<90 && avg>=80)
                    return 'E';
                if(avg<80 && avg>=70)
                    return 'A';
                if(avg<70 && avg>=55)
                    return 'P';
                if(avg<55 && avg>=40)
                    return 'D';
                return 'T';

---
+++ Student_Person.cpp
@@ -0,0 +1,141 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class Person{
+	protected:
+		string firstName;class Student :  public Person{
+	private:
+		vector<int> testScores;  
+	public:
+        /*	
+        *   Class Constructor
+        *   
+        *   Parameters:
+        *   firstName - A string denoting the Person's first name.
+        *   lastName - A string denoting the Person's last name.
+        *   id - An integer denoting the Person's ID number.
+        *   scores - An array of integers denoting the Person's test scores.
+        */
+        // Write your constructor here
+        Student(string firstName,string lastName,int id,vector<int> scores):Person(firstName,lastName,id)
+        {
+            testScores=scores;
+        }    
+        /*	
+        *   Function Name: calculate
+        *   Return: A character denoting the grade.
+        */
+        // Write your function here
+        char calculate()
+        {
+            int tot=0;
+            for(int i=0;i<testScores.size();i++)
+                tot+=testScores[i];
+            float avg=(float)tot/testScores.size();
+            if(avg<=100 && avg>=90)
+                return 'O';
+            if(avg<90 && avg>=80)
+                return 'E';
+            if(avg<80 && avg>=70)
+                return 'A';
+            if(avg<70 && avg>=55)
+                return 'P';
+            if(avg<55 && avg>=40)
+                return 'D';
+            return 'T';
+        }
+};
+
+int main() {
+	string firstName;
+  	string lastName;
+	int id;
+  	int numScores;
+	cin >> firstName >> lastName >> id >> numScores;
+  	vector<int> scores;
+  	for(int i = 0; i < numScores; i++){
+	  	int tmpScore;
+	  	cin >> tmpScore;
+		scores.push_back(tmpScore);
+	}
+	Student* s = new Student(firstName, lastName, id, scores);
+	s->printPerson();
+	cout << "Grade: " << s->calculate() << "\n";
+	return 0;
+}
+		string lastName;
+		int id;
+	public:
+		Person(string firstName, string lastName, int identification){
+			this->firstName = firstName;
+			this->lastName = lastName;
+			this->id = identification;
+		}
+		void printPerson(){
+			cout<< "Name: "<< lastName << ", "<< firstName <<"\nID: "<< id << "\n"; 
+		}
+	
+};
+class Student :  public Person{
+	private:
+		vector<int> testScores;  
+	public:
+        /*	
+        *   Class Constructor
+        *   
+        *   Parameters:
+        *   firstName - A string denoting the Person's first name.
+        *   lastName - A string denoting the Person's last name.
+        *   id - An integer denoting the Person's ID number.
+        *   scores - An array of integers denoting the Person's test scores.
+        */
+        // Write your constructor here
+        Student(string firstName,string lastName,int id,vector<int> scores):Person(firstName,lastName,id)
+        {
+            testScores=scores;
+        }    
+        /*	
+        *   Function Name: calculate
+        *   Return: A character denoting the grade.
+        */
+        // Write your function here
+        char calculate()
+        {
+            int tot=0;
+            for(int i=0;i<testScores.size();i++)
+                tot+=testScores[i];
+            float avg=(float)tot/testScores.size();
+            if(avg<=100 && avg>=90)
+                return 'O';
+            if(avg<90 && avg>=80)
+                return 'E';
+            if(avg<80 && avg>=70)
+                return 'A';
+            if(avg<70 && avg>=55)
+                return 'P';
+            if(avg<55 && avg>=40)
+                return 'D';
+            return 'T';
+        }
+};
+
+int main() {
+	string firstName;
+  	string lastName;
+	int id;
+  	int numScores;
+	cin >> firstName >> lastName >> id >> numScores;
+  	vector<int> scores;
+  	for(int i = 0; i < numScores; i++){
+	  	int tmpScore;
+	  	cin >> tmpScore;
+		scores.push_back(tmpScore);
+	}
+	Student* s = new Student(firstName, lastName, id, scores);
+	s->printPerson();
+	cout << "Grade: " << s->calculate() << "\n";
+	return 0;
+}
